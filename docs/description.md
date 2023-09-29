6fcea9975a19309edabb4226e6dce00ae6b8f3df
Juliano Sathler <jsathler@live.com>
2023-09-29 14:27:19 -0300

    Added terraform-docs action

---
+++ docs/description.md
@@ -0,0 +1,8 @@
+# Azure DNS Private Resolver Terraform module
+
+Terraform module which creates Azure DNS Private Resolver resources on Azure.
+
+Supported Azure services:
+
+* [Azure DNS Private Resolver](https://learn.microsoft.com/en-us/azure/dns/dns-private-resolver-overview)
+* [Azure DNS Private Resolver endpoints and rulesets](https://learn.microsoft.com/en-us/azure/dns/private-resolver-endpoints-rulesets)
--- docs/sample.tf
+++ docs/sample.tf
@@ -1,19 +1,3 @@
-# Azure DNS Private Resolver Terraform module
-
-Terraform module which creates Azure DNS Private Resolver resources on Azure.
-
-These types of resources are supported:
-
-* [Azure DNS Private Resolver](https://learn.microsoft.com/en-us/azure/dns/dns-private-resolver-overview)
-* [Azure DNS Private Resolver endpoints and rulesets](https://learn.microsoft.com/en-us/azure/dns/private-resolver-endpoints-rulesets)
-
-## Terraform versions
-
-Terraform 1.5.6 and newer.
-
-## Usage
-
-```hcl
 module "private-resolver" {
   source              = "jsathler/dns-private-resolver/azurerm"
   name                = "private-resolver"
@@ -49,6 +33,3 @@ module "private-resolver" {
     }
   }
 }
-```
-
-More samples in examples folder
